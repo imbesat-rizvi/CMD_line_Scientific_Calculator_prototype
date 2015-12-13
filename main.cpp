b59fb2973e2c39bf3f82211f3340edecf5454431
ImbesatRizvi <imbugene@gmail.com>
2015-12-14 05:15:03 +0530

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
 	complex<long double> calculated_result;
 
 	if(argc !=2)
-		printf("ERROR\n");
+		printf("Only one command line argument should be there.\n");
 
 	calculated_result = Calc.calculate(argv[1]);
 	print_complex(calculated_result);
